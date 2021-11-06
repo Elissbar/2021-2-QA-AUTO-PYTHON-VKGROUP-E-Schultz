e556424c6237d74f84ce7fdac043441f225da00d
Elissbar <allen-2002@mail.ru>
2021-11-06 23:18:46 +0300

    added changes

--- HW-3-API-Tests/base.py
+++ HW-3-API-Tests/base.py
@@ -1,5 +1,7 @@
 from client import ApiClient
 from _pytest.fixtures import FixtureRequest
+from payloads.create_campaign import Payloads
+from urllib.parse import urljoin
 from faker import Faker
 import pytest
 import os
@@ -10,11 +12,12 @@ class BaseCase:
 
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, request: FixtureRequest):
-        self.api_client = ApiClient()
+        self.api_client = ApiClient('https://target.my.com/')
+        self.payloads = Payloads()
 
         if self.authorize:
             credentials = request.getfixturevalue('credentials')
-            self.api_client.login(*credentials)
+            self.api_client.post_login(*credentials)
 
     @pytest.fixture(scope='session')
     def credentials(self, repo_root):
@@ -28,6 +31,22 @@ class BaseCase:
         faker = Faker()
         name = faker.lexify(text='????????????')
         return name
+
+    @pytest.fixture(scope='function')
+    def post_create_campaign(self, file_path, fake_data):
+        file_id = self.api_client.post_upload_file(file_path)
+        location = 'api/v2/campaigns.json'
+        headers = {
+            'X-CSRFToken': self.api_client.csrf_token
+        }
+        payload = self.payloads.campaign_payload(campaign_name=fake_data, file_id=file_id)
+        result = self.api_client.session.post(urljoin(self.api_client.base_url, location), headers=headers, json=payload)
+        yield result.json()['id']
+        location = f'api/v2/campaigns/{result.json()["id"]}.json'
+        headers = {
+            'X-CSRFToken': self.api_client.csrf_token
+        }
+        return self.api_client.session.delete(urljoin(self.api_client.base_url, location), headers=headers)
 
 
 
--- HW-3-API-Tests/test_api.py
+++ HW-3-API-Tests/test_api.py
@@ -6,17 +6,15 @@ import pytest
 class TestApi(BaseCase):
     authorize = True
 
-    def test_create_campaign(self, file_path, fake_data):
-        campaign_id = self.api_client.create_campaign(file_path=file_path, campaign_name=fake_data)
-        assert self.api_client.check_campaign(campaign_id=campaign_id)
-        self.api_client.delete_campaign(campaign_id=campaign_id)
+    def test_create_campaign(self, post_create_campaign, file_path, fake_data):
+        assert self.api_client.get_check_campaign(campaign_id=post_create_campaign)
 
     def test_create_segment(self, fake_data):
-        id_segment = self.api_client.create_segment(segment_name=fake_data)
-        assert self.api_client.check_segment(id_segment=id_segment)
-        self.api_client.delete_segment(id_segment=id_segment)
+        id_segment = self.api_client.post_create_segment(segment_name=fake_data)
+        assert self.api_client.get_check_segment(id_segment=id_segment)
+        self.api_client.delete_delete_segment(id_segment=id_segment)
 
     def test_delete_segment(self, fake_data):
-        id_segment = self.api_client.create_segment(segment_name=fake_data)
-        self.api_client.delete_segment(id_segment=id_segment)
-        assert not self.api_client.check_segment(id_segment=id_segment)
+        id_segment = self.api_client.post_create_segment(segment_name=fake_data)
+        self.api_client.delete_delete_segment(id_segment=id_segment)
+        assert not self.api_client.get_check_segment(id_segment=id_segment)
